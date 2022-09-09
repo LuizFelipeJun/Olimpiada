658b66819c84a686c44913bab517efbfb09ca926
LuizFelipeJun <104800410+LuizFelipeJun@users.noreply.github.com>
2022-09-09 19:16:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,32 +4,32 @@ ctx= canvas.getContext("2d");
 
 ctx.beginPath();
 ctx.strokeStyle = "blue";
-ctx.linewidth = 5;
+ctx.lineWidth = 10;
 ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 10;
+ctx.arc(360, 210, 40, 0, 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "red";
 ctx.linewidth = 5;
-ctx.arc(300, 210, 40, 0, 2 * Math.PI);
+ctx.arc(470, 210, 40, 0, 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "green";
 ctx.linewidth = 5;
-ctx.arc(350, 210, 40, 0, 2 * Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "black";
-ctx.linewidth = 5;
-ctx.arc(330, 260, 40, 0, 2 * Math.PI);
+ctx.arc(420, 260, 40, 0, 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "yellow";
 ctx.linewidth = 5;
-ctx.arc(270, 260, 40, 0, 2 * Math.PI);
+ctx.arc(300, 260, 40, 0, 2 * Math.PI);
 ctx.stroke();
 
 
